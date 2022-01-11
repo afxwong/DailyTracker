5d2351010049b720183c0a20cec5ec78ea493481
Anthony Wong <afxwong@gmail.com>
2022-01-10 23:22:25 -0500

    add columns and switch plotting mechanism

--- GenericLinePlot.py
+++ GenericLinePlot.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 
@@ -9,5 +10,7 @@ class GenericLinePlot:
     def plot(self):
         pd.options.plotting.backend = "plotly"
         df = pd.read_csv("data.csv")
-        fig = px.line(df, x="date", y=self.title, markers=True)
+        # fig = px.line(df, x="date", y=self.title, markers=True)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df["date"], y=df[self.title], connectgaps=True, hovertext=df[self.title]))
         fig.write_html(f'templates/{self.title}.html')
